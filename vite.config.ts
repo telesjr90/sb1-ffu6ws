f3126c8a2e2471147bf8ad283dfddc3a321982c2
Teles Santos <72049896+telesjr90@users.noreply.github.com>
2024-11-13 09:50:09 -0800

    Corrige configuração do TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 3000
-  },
-  define: {
-    'process.env': {}
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
